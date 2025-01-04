ebc63be04f73f3aaabc2a7c7230046f42e7cb3a4
ChrisplayzYT/BLOX <chrisplayzblxthesecondxd@gmail.com>
2025-01-04 13:25:54 -0600

    wowie so much effort!1!1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+function onload() {
+    document.getElementById("loadicon").style.display="none";
+  }
+
+function initloading() {
+    document.getElementById("loadicon").style.display="block";
+    setTimeout(stoploading,4500); // if page dont load past this point loader stops xD
+    document.getElementById("loadingsound").play();
+    document.getElementById("loadingsound").currentTime = 0;
+}
+
+function stoploading() {
+    document.getElementById("loadicon").style.display="none";
+    document.getElementById("loadingsound").pause();
+}
+
+function wiihover() {
+    var hoverwii = new Audio("/sound/wiishopsounds/2.wav");
+    hoverwii.play();
+}
